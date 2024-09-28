0ba0ab668cd4d24abaea858569b913eb23bac866
Vinicyos Ferreira <vinifr987@gmail.com>
2024-09-27 21:27:32 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+
+  return (
+    <header>
+        <i>Icone</i>
+        <h1>Dev Weather</h1>
+        <nav>
+            <a>Home</a>
+            <a>Sobre</a>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
